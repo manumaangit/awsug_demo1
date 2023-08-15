41c26f1ca8ae2fdc29d2e638216c13bdc1c14a82
Ubuntu <ubuntu@ip-172-31-40-158.ec2.internal>
2023-08-15 16:09:38 +0000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import uc = require("upper-case");
+import http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Button Page</title>
+            </head>
+            <body>
+                <button>Buy 1 day</button>
+                <button>Buy 7 days</button>
+            </body>
+            </html>
+        `);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Page not found');
+    }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
