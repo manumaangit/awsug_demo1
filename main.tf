bf89cd9ade9e1f2fdcb159b2e9604eabcb2e6972
mm00922318 <mm00922318@techmahindra.com>
2023-08-18 10:31:38 +0530

    userdata test1

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "tls_private_key" "terrafrom_generated_private_key" {
 }
 
 resource "aws_security_group" "ssh-sec-grp" {
-  name        = "ssh-sec-grp-tf1"
+  name        = "ssh-sec-grp-tfa"
   description = "Allow SSH traffic via Terraform"
 
   ingress {
@@ -34,7 +34,7 @@ resource "aws_security_group" "ssh-sec-grp" {
 resource "aws_key_pair" "generated_key" {
 
   # Name of key: Write the custom name of your key
-  key_name = "aws_keys_pairs-tf1"
+  key_name = "aws_keys_pairs-tfa"
 
   # Public Key: The public will be generated using the reference of tls_private_key.terrafrom_generated_private_key
   public_key = tls_private_key.terrafrom_generated_private_key.public_key_openssh
@@ -56,12 +56,12 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 resource "aws_instance" "ec2_instance" {
   ami                    = "ami-053b0d53c279acc90" # Change this to the correct Ubuntu 20.04 AMI ID
   instance_type          = "t2.micro"              # Change to the desired instance type
-  key_name               = "aws_keys_pairs-tf1"    # Change to your key pair name
+  key_name               = "aws_keys_pairs-tfa"    # Change to your key pair name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   vpc_security_group_ids = [aws_security_group.ssh-sec-grp.id]
   tags = {
-    Name = "Ubuntu-Docker-Instance=tf"
+    Name = "Ubuntu-Docker-Instance=tfa"
   }
   connection {
     type = "ssh"
