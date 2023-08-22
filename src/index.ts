5ded503bce6bc5b40d2a9a3673a323ec0f211e42
mm00922318 <mm00922318@techmahindra.com>
2023-08-22 23:07:10 +0530

    dummy change1

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ const server = http.createServer((req, res) => {
                 <title>Button Page</title>
             </head>
             <body>
-                <button>Buy 101 day</button>
-                <button>Buy 201 days</button>
+                <button>Buy 1 day</button>
+                <button>Buy 2 days</button>
             </body>
             </html>
         `);
