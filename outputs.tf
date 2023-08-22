c30ab4d5ca60ab164e33864bb9139ed57a4c43d4
mm00922318 <mm00922318@techmahindra.com>
2023-08-22 17:44:25 +0530

    tf workflow changes

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "ec2_public_ip" {
   description = "Public IP of the instance"
   value       = aws_instance.ec2_instance.*.public_ip
 }
+
+output "ec2_private_key" {
+  description = "Private Key of the instance"
+  value       = tls_private_key.terrafrom_generated_private_key.private_key_pem
+}
